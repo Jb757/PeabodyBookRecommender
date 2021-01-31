c80b8a38b289500e8a57589f1bbbff5599db80ce
Josh <josh.bailey757@gmail.com>
2021-01-31 20:00:33 +0000

    another commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, g
 from flask_sqlalchemy import SQLAlchemy
-from peabody.models.tfidf import TfidfRecommender
+from peabody import TfidfRecommender
 
 
 
---
+++ peabody/__init__.py
@@ -0,0 +1,2 @@
+from peabody.models import TfidfRecommender, Doc2VecRecommender, Recommender
+from peabody.utils import preprocess, list_for_vectorisation, tokenised_summary
